001c5b4607f9344d1f2616c67c876ad4c042f006
AleksandrEmchenko <aleksandremchenko@gmail.com>
2022-12-23 21:50:38 +0200

    add modal

--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-(() => {
-  const refs = {
-    openMapModalBtn: document.querySelector("[data-map-modal-open]"),
-    closeMapModalBtn: document.querySelector("[data-map-modal-close]"),
-    mapModal: document.querySelector("[data-map-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openMapModalBtn: document.querySelector("[data-map-modal-open]"),
+//     closeMapModalBtn: document.querySelector("[data-map-modal-close]"),
+//     mapModal: document.querySelector("[data-map-modal]"),
+//   };
 
-  refs.openMapModalBtn.addEventListener("click", toggleMapModal);
-  refs.closeMapModalBtn.addEventListener("click", toggleMapModal);
+//   refs.openMapModalBtn.addEventListener("click", toggleMapModal);
+//   refs.closeMapModalBtn.addEventListener("click", toggleMapModal);
 
-  function toggleMapModal() {
-    refs.mapModal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleMapModal() {
+//     refs.mapModal.classList.toggle("is-hidden");
+//   }
+// })();
 
-(() => {
-  const refs = {
-    openReadModalBtn: document.querySelector("[data-read-modal-open]"),
-    closeReadModalBtn: document.querySelector("[data-read-modal-close]"),
-    readModal: document.querySelector("[data-read-modal]"),
-  };
+// (() => {
+//   const refs = {
+//     openReadModalBtn: document.querySelector("[data-read-modal-open]"),
+//     closeReadModalBtn: document.querySelector("[data-read-modal-close]"),
+//     readModal: document.querySelector("[data-read-modal]"),
+//   };
 
-  refs.openReadModalBtn.addEventListener("click", toggleReadModal);
-  refs.closeReadModalBtn.addEventListener("click", toggleReadModal);
+//   refs.openReadModalBtn.addEventListener("click", toggleReadModal);
+//   refs.closeReadModalBtn.addEventListener("click", toggleReadModal);
 
-  function toggleReadModal() {
-    refs.readModal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleReadModal() {
+//     refs.readModal.classList.toggle("is-hidden");
+//   }
+// })();
 
 
